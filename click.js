6c9d2e1278af79d7edf54c361c0a53c570b4b1b1
Hafees S <hafees15hafi@gmail.com>
2022-04-22 15:00:51 +0530

    Moved reusable steps into a new function

--- click.js
+++ click.js
@@ -2,6 +2,8 @@ import { getImagedetails, createImage, loadingTime, afterLoad, buttonDisabler, b
 
 const btn1 = document.querySelector('#previousBtn');
 const btn2 = document.querySelector('#nextBtn');
+const next="next";
+const prev="previous";
 
 let currentId = 1;
 const minIdPossible = 1;
@@ -14,20 +16,17 @@ const defaultPage = async () => {
     afterLoad();
     createImage(preloadImage.url);
 }
-
-
 //function to return next image id
 function loadnextId() {
     if (currentId == maxIdPossible) {
         currentId = currentId;
-        buttonDisabler("next");
+        buttonDisabler(next);
     } else {
         currentId++;
-        buttonEnabler("previous");
+        buttonEnabler(prev);
     }
     return currentId;
 }
-
 //function to return previous image id
 function loadpreviousId() {
     if (currentId == minIdPossible) {
@@ -37,49 +36,42 @@ function loadpreviousId() {
     }
     return currentId;
 }
-
 //Limit checker
 function maxLimitCheck(){
     if (currentId==maxIdPossible) {
-        buttonDisabler("next");
+        buttonDisabler(next);
     } else {
-        buttonEnabler("next");
+        buttonEnabler(next);
     }
 };
 function minLimitCheck(){
     if (currentId==minIdPossible) {
-        buttonDisabler("previous");
+        buttonDisabler(prev);
     } else {
-        buttonEnabler("previous");
+        buttonEnabler(prev);
     }
 }
-
+// common btn functions
+async function btnClick(type){
+    loadingTime();
+    buttonDisabler(type);
+    const imageDetails = await getImagedetails(currentId);
+    afterLoad();
+    maxLimitCheck();
+    minLimitCheck();
+    createImage(imageDetails.url);
+    indexNo(currentId);
+}
 //next button handler
 const nextImage = async () => {
     loadnextId();
-    loadingTime();
-    buttonDisabler("next");
-    const imageDetails = await getImagedetails(currentId);
-    afterLoad();
-    maxLimitCheck();
-    minLimitCheck();
-    createImage(imageDetails.url);
-    indexNo(currentId);
+    btnClick(next);
 }
-
 //previous button handler
 const previousImage = async () => {
     loadpreviousId();
-    loadingTime();
-    buttonDisabler("previous");
-    const imageDetails = await getImagedetails(currentId);
-    createImage(imageDetails.url);
-    afterLoad();
-    minLimitCheck();
-    maxLimitCheck();
-    indexNo(currentId);
+    btnClick(prev);
 }
-
 // function to show index no
 function indexNo(currentId) {
     const indexText = document.getElementById('indexNo');
